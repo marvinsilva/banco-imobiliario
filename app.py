441a2e2a0f76acb05db325e59b461a2cdecbebbe
marvinsilva <vnc_vinicius92@hotmail.com>
2020-02-03 01:03:51 -0300

    Separando a função de opcoes_comando no arquivo de comandos

--- app.py
+++ app.py
@@ -7,59 +7,16 @@
 """
 
 import random
-import os
 import sys
-import getopt
+import os
 from src.principal import executar_simulacoes, tratamento_de_dados
+from src.comandos import opcoes_comando
 
-__author__ = "[marvinsilva] Marcus Vinicius Laurindo da Silva"
-__license__ = "GNU General Public License v3.0"
-__version__ = "1.0.0"
-__status__ = "Production"
 file_name = os.path.basename(__file__)
-
-mensagem_ajuda = f'\nUso: python {file_name} [opções] ... \
-                    \n[-e --exec | -v --version | -h --help]\
-                    \nopçoes e argumentoss:\
-                    \n-e   :   executa as simulações do jogo banco imobiliário \
-                    \n-v   :   versão do software\
-                    \n-h   :   mensagem de ajuda sobre o software\n\
-                    \nPara testar use o seguinte comando no diretório raiz deste projeto:\
-                    \n$ python -m unittest discover'
-
-def opcoes_comando(argument):
-    """ Parses the command line options and the parameter list
-    """
-    try:
-        opts, args = getopt.getopt(argument[1:], "evh", ["exec=", "version", "help"])
-
-        if len(argument) < 2 or not opts:
-            print(f'Erro: uso {file_name} <opcoes -e -h -v>')
-            sys.exit(2)
-
-        for opt, arg in opts:
-            if opt in ("-e", "--exec"):
-                return True
-            elif opt in ("-h", "--help"):
-                print(mensagem_ajuda)
-                sys.exit()
-            elif opt in ("-v", "--version"):
-                print(f'Version: {__version__}\n'
-                      f'Author: {__author__}\n'
-                      f'License: {__license__}\n'
-                      f'Status: {__status__}')
-                sys.exit()
-            else:
-                assert False, "Comando não reconhecido"
-
-    except getopt.GetoptError as err:
-        print(f'Erro: {err}')
-        sys.exit(2)
-
 
 if(__name__ == "__main__"):
 
-    if opcoes_comando(sys.argv):
+    if opcoes_comando(sys.argv, file_name):
 
         # Paramêtros para simulação do jogo banco imobiliário
         perfis = ['impulsivo', 'exigente', 'cauteloso', 'aleatorio']
---
+++ src/comandos.py
@@ -0,0 +1,46 @@
+import sys
+import getopt
+
+__author__ = "[marvinsilva] Marcus Vinicius Laurindo da Silva"
+__license__ = "GNU General Public License v3.0"
+__version__ = "1.0.0"
+__status__ = "Production"
+
+
+def opcoes_comando(argument, file_name):
+    """ Função responsável por tratar os parâmetros do comando de entrada
+    """
+
+    mensagem_ajuda = f'\nUso: python {file_name} [opções] ... \
+                        \n[-e --exec | -v --version | -h --help]\
+                        \nopçoes e argumentoss:\
+                        \n-e   :   executa as simulações do jogo banco imobiliário \
+                        \n-v   :   versão do software\
+                        \n-h   :   mensagem de ajuda sobre o software\n\
+                        \nPara testar use o seguinte comando no diretório raiz deste projeto:\
+                        \n$ python -m unittest discover'
+
+    try:
+        opts, args = getopt.getopt(argument[1:], "evh", ["exec=", "version", "help"])
+        if len(argument) < 2 or not opts:
+            print(f'Erro: uso python {file_name} <opcoes -e -h -v>')
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt in ("-e", "--exec"):
+                return True
+            elif opt in ("-h", "--help"):
+                print(mensagem_ajuda)
+                sys.exit()
+            elif opt in ("-v", "--version"):
+                print(f'Version: {__version__}\n'
+                      f'Author: {__author__}\n'
+                      f'License: {__license__}\n'
+                      f'Status: {__status__}')
+                sys.exit()
+            else:
+                assert False, "Comando não reconhecido"
+
+    except getopt.GetoptError as err:
+        print(f'Error: {err}')
+        sys.exit(2)
+
